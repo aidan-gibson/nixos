65c5dae173fc27a0d6d0071b7ca8922ebfa4ebcb
Aidan Gibson <tronicdude@gmail.com>
2023-11-06 15:42:40 -0800

    flake systemname update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       forAllSystems = nixpkgs.lib.genAttrs allSystems;
     in {
       nixosConfigurations = {
-        mySystemName = nixpkgs.lib.nixosSystem {
+        trix = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             ./configuration.nix
