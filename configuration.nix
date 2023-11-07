fc0d49e732a58f0fbdf69c35b9a21d89fff95ce0
Aidan Gibson <tronicdude@gmail.com>
2023-11-06 23:59:31 -0800

    boot options cleanup

--- configuration.nix
+++ configuration.nix
@@ -34,7 +34,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # now in boot.nix
+
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
