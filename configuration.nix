653fabd66e8287cdae67ddd0e76965779841879a
Aidan Gibson <tronicdude@gmail.com>
2023-11-06 17:29:48 -0800

    syntax

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
 # and in the NixOS manual (accessible by running `nixos-help`).
 
 { config, pkgs, lib, options, inputs, ... }:
-
+{
 # https://nixos.wiki/wiki/Hardware/Apple auto restart
 # with lib;
 
