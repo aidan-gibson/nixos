1ef0fbfa3b58f88a8073b61c60eb548264acfc25
Aidan Gibson <tronicdude@gmail.com>
2023-11-06 17:21:56 -0800

    commented out autoreboot

--- configuration.nix
+++ configuration.nix
@@ -6,32 +6,32 @@
 { config, pkgs, lib, options, inputs, ... }:
 
 # https://nixos.wiki/wiki/Hardware/Apple auto restart
-with lib;
+# with lib;
 
-let
-  register = {
-    "mini_white_intel+nVidia" = "00:03.0 0x7b.b=0x19";
-    "mini_white_intel" = "0:1f.0 0xa4.b=0";
-    "mini_unibody_intel" = "0:3.0 -0x7b=20";
-    "mini_unibody_M1" = "?";
-  };
+# let
+#   register = {
+#     "mini_white_intel+nVidia" = "00:03.0 0x7b.b=0x19";
+#     "mini_white_intel" = "0:1f.0 0xa4.b=0";
+#     "mini_unibody_intel" = "0:3.0 -0x7b=20";
+#     "mini_unibody_M1" = "?";
+#   };
 
-in {
-  options.hardware.macVariant = mkOption {
-    type = types.enum (attrNames register);
-    default = elemAt (attrNames register) 0;
-    example = elemAt (attrNames register) 0;
-    description =
-      "Minor hardware variants have different registers for enabling autostart";
-  };
-  # Needs to run every reboot
-  systemd.services.enable-autorestart = {
-    script = ("${pkgs.pciutils}/bin/setpci -s "
-      + (getAttr config.hardware.macVariant register));
-    wantedBy = [ "default.target" ];
-    after = [ "default.target" ];
-  };
-  hardware.macVariant = "mini_unibody_intel";
+# in {
+#   options.hardware.macVariant = mkOption {
+#     type = types.enum (attrNames register);
+#     default = elemAt (attrNames register) 0;
+#     example = elemAt (attrNames register) 0;
+#     description =
+#       "Minor hardware variants have different registers for enabling autostart";
+#   };
+#   # Needs to run every reboot
+#   systemd.services.enable-autorestart = {
+#     script = ("${pkgs.pciutils}/bin/setpci -s "
+#       + (getAttr config.hardware.macVariant register));
+#     wantedBy = [ "default.target" ];
+#     after = [ "default.target" ];
+#   };
+#   hardware.macVariant = "mini_unibody_intel";
 
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
